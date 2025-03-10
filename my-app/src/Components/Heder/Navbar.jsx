929a1a5a2ce313800856d8fe816ea6cb88d883db
Robertas_Vasiliauskas <robertaswasiliauskas@gmail.com>
2025-03-10 21:45:14 +0100

    made full Navbar.jsx component

--- my-app/src/Components/Heder/Navbar.jsx
+++ my-app/src/Components/Heder/Navbar.jsx
@@ -1,19 +1,21 @@
+import { useState } from 'react';
+import Navitem from './Navitem.jsx';
+
+import icon_feed from '../../assets/Feather.svg';
+import icon_convert from '../../assets/Divide_square.svg';
+import icon_compare from '../../assets/Book_open.svg';
+import icon_dashboard from '../../assets/Sidebar.svg';
+
 export default function Navbar() {
+    const [activeTab, setActiveTab] = useState('Dashboard');
+
     return (
-        <nav>
-            <ul className={"bg-[#1A2E40] flex gap-4 h-[5.625rem] w-[25rem] rounded-[15px] items-center justify-center"}>
-                <li>
-                    <a href={"#"}>Dashboard</a>
-                </li>
-                <li>
-                    <a href={"#"}>Convert</a>
-                </li>
-                <li>
-                    <a href={"#"}>Compare</a>
-                </li>
-                <li>
-                    <a href={"#"}>News</a>
-                </li>
+        <nav className={ "bg-[#1A2E40] flex h-[5.625rem] w-[46rem] rounded-[15px] px-[2rem] justify-center"} >
+            <ul className={"flex grow items-center justify-center h-full"}>
+                <Navitem icon={icon_dashboard} title={"Dashboard"} isActive={activeTab === 'Dashboard'} onClick={() => setActiveTab('Dashboard')} />
+                <Navitem icon={icon_convert} title={"Convert"} isActive={activeTab === 'Convert'} onClick={() => setActiveTab('Convert')} />
+                <Navitem icon={icon_compare} title={"Compare"} isActive={activeTab === 'Compare'} onClick={() => setActiveTab('Compare')} />
+                <Navitem icon={icon_feed} title={"News"} isActive={activeTab === 'News'} onClick={() => setActiveTab('News')} />
             </ul>
         </nav>
     );
