b703cec2b98d3661c301c141a77d790234827e3b
Robertas_Vasiliauskas <robertaswasiliauskas@gmail.com>
2025-03-10 23:08:36 +0100

    Created Button.jsx component

--- my-app/src/Components/Heder/Header.jsx
+++ my-app/src/Components/Heder/Header.jsx
@@ -1,4 +1,5 @@
 import logo from '../../assets/logo.svg'
+import Button from '../Button.jsx'
 import Navbar from './Navbar.jsx';
 
 export default function Header() {
@@ -6,7 +7,9 @@ export default function Header() {
         <header className={"h-[11rem] flex items-center justify-between px-[2rem]"}>
             <img src={logo} alt={"logo"} className={"h-10 invert-100 h-[10rem] w-[10rem]"}/>
             <Navbar />
-            <h1>Hello</h1>
+            <div>
+                <Button text={"Login"} className={"text-4xl text-primary px-4 py-2 rounded-md w-[13.75rem] h-[4.25rem]"} />
+            </div>
         </header>
     )
 }
