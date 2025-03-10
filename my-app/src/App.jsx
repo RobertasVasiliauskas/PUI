529481cd073e20f82465bd8d65860de508d68db4
jakub <273124@student.pwr.edu.pl>
2025-03-10 23:01:52 +0100

    explore list

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './Components/Heder/Header.jsx'
 import Carousel from './Components/Dashboard/Carousel.jsx'
+import ExploreList from "./Components/Dashboard/ExploreList.jsx";
 
 function App() {
     return (
         <div className={"flex flex-col flex-1 min-h-screen"}>
             <Header/>
             <Carousel/>
+            <ExploreList/>
         </div>
     );
 }
